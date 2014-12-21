2be85a2649d56bcaef4db61dc6c5f98e681b42c0
MBeckt <mrcatmunch@gmail.com>
2014-12-21 20:57:09 +0000

    try extern

--- Source.cpp
+++ Source.cpp
@@ -10,4 +10,7 @@ int main(){
 	std::cin >> refVal;
 	std::cout << ::val << std::endl; // Using MS Visual to write this but im debugging in codeblocks for simplicity.
 	std::cout << val << std::endl;  // '::' Should mean that whilst :: is there val will = 6 no matter the input.
+
+	extern int i;
+	std::cout << i << std::endl; // This should bring the 'I' from the other document.
 }
