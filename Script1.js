61d92452b94f2211bd06656f79479665b2f5b428
MBeckt <mrcatmunch@gmail.com>
2014-12-21 16:10:02 +0000

    Added some pretty useless jQuery :D

--- Script1.js
+++ Script1.js
@@ -1,4 +1,10 @@
 // JavaScript source code
-confirm("Continu 2 Shiz Pague?")
 
-console.log(confirm)
+$(document).ready(function () {
+    $('#Block1').mouseenter(function () {
+        $('#Block1').fadeTo('slow', 0);
+    });
+    $('#Block1').mouseleave(function () {
+        $('#Block1').fadeTo('slow', 1);
+    });
+});
