7ee38c2a39b48e44028b023f8b94775618865a28
MBeckt <mrcatmunch@gmail.com>
2014-12-21 16:30:10 +0000

    Additional jQuery, 2nd Stylesheet for CSS mess.

--- Script1.js
+++ Script1.js
@@ -7,4 +7,28 @@ $(document).ready(function () {
     $('#Block1').mouseleave(function () {
         $('#Block1').fadeTo('slow', 1);
     });
+    $('#Block2').mouseenter(function () {
+        $('#Block2').fadeTo('slow', 0);
+    });
+    $('#Block2').mouseleave(function () {
+        $('#Block2').fadeTo('slow', 1);
+    });
+    $('#Block3').mouseenter(function () {
+        $('#Block3').fadeTo('slow', 0);
+    });
+    $('#Block3').mouseleave(function () {
+        $('#Block3').fadeTo('slow', 1);
+    });
+    $('#Block4').mouseenter(function () {
+        $('#Block4').fadeTo('slow', 0);
+    });
+    $('#Block4').mouseleave(function () {
+        $('#Block4').fadeTo('slow', 1);
+    });
+    $('#Block5').mouseenter(function () {
+        $('#Block5').fadeTo('slow', 0);
+    });
+    $('#Block5').mouseleave(function () {
+        $('#Block5').fadeTo('slow', 1);
+    });
 });
