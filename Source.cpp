62340d3b3f09a60c62ac4ecaf6a181fc84964a8b
MBeckt <mrcatmunch@gmail.com>
2014-12-21 19:47:51 +0000

    Pointer test

--- Source.cpp
+++ Source.cpp
@@ -6,4 +6,7 @@ using namespace std;
 int main(){
 	short val = 5;
 	&refVal = val;
+
+	std::cin >> refVal;
+	std::cout << val << std::endl; // Using MS Visual to write this but im debugging in codeblocks for simplicity.
 }
