c9a773a3cbb24112bbf4fa1a3c6e289659a09a87
MBeckt <mrcatmunch@gmail.com>
2014-12-22 06:25:36 +0000

    added some output

--- Source.cpp
+++ Source.cpp
@@ -7,12 +7,14 @@ int main(){
 	short val = 5;
 	short &refVal = val;
 
+	std::cout << " Type a number to change the value of val" << std::endl;
 	std::cin >> refVal;
 	std::cout << ::val << std::endl; // Using MS Visual to write this but im debugging in codeblocks for simplicity.
 	std::cout << val << std::endl;  // '::' Should mean that whilst :: is there val will = 6 no matter the input.
 
 	extern int i;
 	std::cout << i << std::endl; // This should bring the 'I' from the other document.
+	return 0;
 }
 
-/*this is all just for playing with some ketwords and getting more used to them(extern)*/
+/*this is just for playing with some ketwords and getting more used to them*/
